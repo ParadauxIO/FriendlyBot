e2461fc0563d4aa92d6e4680e37f89878d779da6
Rían Errity <rian@paradayx.io>
2020-11-01 22:00:54 +0000

    Proper logging

--- src/main/java/io/paradaux/csbot/CSBot.java
+++ src/main/java/io/paradaux/csbot/CSBot.java
@@ -2,9 +2,12 @@ package io.paradaux.csbot;
 
 import io.paradaux.csbot.api.ConfigurationCache;
 import io.paradaux.csbot.api.ConfigurationUtils;
+import io.paradaux.csbot.api.Logging;
 import net.dv8tion.jda.api.JDA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
 
 /**
  * CSBot is the main (executable) class for the project. It provides a set of lazy dependency injection instances
@@ -19,19 +22,26 @@ public class CSBot {
     private static JDA jda;
     public static JDA getJda() { return jda; }
 
-    private static Logger logger;
-    public static Logger getLogger() { return logger; }
-
     private static ConfigurationCache configurationCache;
     public static ConfigurationCache getConfigurationCache() { return configurationCache; }
 
     public static void main(String[] args) {
-        logger = LoggerFactory.getLogger("CSBot");
+
+        // Instantiate Logger instance.
+        Logging logging = new Logging();
+        Logger logger = Logging.getLogger();
+
+        // Version Information
         System.out.println("CSBot v0.1.0 - Maintained by Rían Errity <rian@paradaux.io>");
 
+        // Prepare Configuration File/Cache
         ConfigurationUtils.deployConfiguration();
+        try {
+            configurationCache = ConfigurationUtils.readConfigurationFile();
+        } catch (FileNotFoundException exception) {
+            logger.error("Could not find the configuration file!", exception);
+        }
+
     }
-
-
 
 }
