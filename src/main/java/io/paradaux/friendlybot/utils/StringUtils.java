12d3fa46b64964ef57f81969cc6b06fb73fb6697
Rían Errity <rian@paradaux.io>
2021-01-19 15:34:41 +0000

    String utils test

--- src/main/java/io/paradaux/friendlybot/utils/StringUtils.java
+++ src/main/java/io/paradaux/friendlybot/utils/StringUtils.java
@@ -21,7 +21,7 @@
  * See LICENSE.md for more details.
  */
 
-package io.paradaux.friendlybot.utils.models;
+package io.paradaux.friendlybot.utils;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
