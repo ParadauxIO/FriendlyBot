b288f51fdeb29d1b48e102e0af302a893ce46190
Rían Errity <rian@paradaux.io>
2021-04-25 00:44:04 +0100

    WIP commad to onboard new guilds

--- src/main/java/io/paradaux/friendlybot/commands/staff/technician/SetupCommand.java
+++ src/main/java/io/paradaux/friendlybot/commands/staff/technician/SetupCommand.java
@@ -3,6 +3,8 @@ package io.paradaux.friendlybot.commands.staff.technician;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import io.paradaux.friendlybot.managers.PermissionManager;
+import io.paradaux.friendlybot.managers.RoleManager;
+import io.paradaux.friendlybot.utils.StringUtils;
 import io.paradaux.friendlybot.utils.models.configuration.ConfigurationEntry;
 import io.paradaux.friendlybot.utils.models.types.PrivilegedCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -31,25 +33,39 @@ public class SetupCommand extends PrivilegedCommand {
 
         EmbedBuilder builder = new EmbedBuilder();
 
-        event.getChannel().sendMessage("Please set the title.").queue(message -> waiter.waitForEvent(MessageReceivedEvent.class,
+        event.getChannel().sendMessage("Are you going to use the custom color roles? (yes, no)").queue(message -> waiter.waitForEvent(MessageReceivedEvent.class,
                 messageEvent -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent -> {
+            boolean usingColorRoles = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
+
+            if (usingColorRoles) {
+                messageEvent.getChannel().sendMessage("Creating a roll called `Color Roles Begin Here`. Please move this role above where you would like color roles to be put. **N.B**: This has to be below the bot's highest role.)");
+                RoleManager.getInstance().setupColorRolePivot(event.getGuild());
+            }
+                event.getChannel().sendMessage("Are you going to use Email Verification? (yes, no)").queue(message2 -> waiter.waitForEvent(MessageReceivedEvent.class,
+                    messageEvent2 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent2 -> {
+                boolean usingEmail = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
+                    event.getChannel().sendMessage("Are you going to use the ModMail ? (yes, no)").queue(message3 -> waiter.waitForEvent(MessageReceivedEvent.class,
+                            messageEvent3 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent3 -> {
+                    boolean usingModMail = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
+
+                            event.getChannel().sendMessage("Are you going to use the custom color roles? (yes, no)").queue(message4 -> waiter.waitForEvent(MessageReceivedEvent.class,
+                                    messageEvent4 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent4 -> {
+
+                                    }));
+
+
+                    }));
+
+                }));
+
 
             // Going to use roles
             // Going to use modmail
             // Going to use email verification
-            // Going to use color roles
-
-
 
 
         }));
     }
-
-
-    public boolean getBoolean(String message) {
-        
-    }
-
 
 
     public boolean sameOrigin(Message message1, Message message2) {
