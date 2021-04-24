46af3c8ef5a591b3e47f4ccc89700a7e09fc0ea6
Rían Errity <rian@paradaux.io>
2021-04-25 00:44:54 +0100

    Make rolemanager more generic

--- src/main/java/io/paradaux/friendlybot/managers/RoleManager.java
+++ src/main/java/io/paradaux/friendlybot/managers/RoleManager.java
@@ -2,8 +2,13 @@ package io.paradaux.friendlybot.managers;
 
 import io.paradaux.friendlybot.utils.models.exceptions.ManagerNotReadyException;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.requests.restaction.RoleAction;
 import org.slf4j.Logger;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RoleManager {
 
@@ -27,6 +32,11 @@ public class RoleManager {
         return instance;
     }
 
+    public void setupColorRolePivot(Guild guild) {
+        Role highestRole = getHighestFrom(guild.getSelfMember());
+        guild.createRole().setName("Color Roles Begin Here").queue((role -> guild.modifyRolePositions().selectPosition(role).moveTo(role.getPosition() - 1).queue()));
+    }
+
     public boolean checkForConflicts(Guild guild, String name) {
         return DiscordBotManager.getInstance().getRolesByName(guild.getId(), name).size() != 0;
     }
@@ -40,7 +50,29 @@ public class RoleManager {
                 .setColor(Integer.parseInt(color, 16))
                 .setMentionable(false)
                 .setName(color);
-
     }
 
+    /**
+     * Determines the highest role (i.e the role displayed when the user speaks) of the member provided.
+     * */
+    @Nullable
+    public Role getHighestFrom(Member member) {
+        if (member == null) {
+            return null;
+        }
+
+        List<Role> roles = member.getRoles();
+
+        if (roles.isEmpty()) {
+            return null;
+        }
+
+        return roles.stream().min((first, second) -> {
+            if (first.getPosition() == second.getPosition()) {
+                return 0;
+            }
+
+            return first.getPosition() > second.getPosition() ? -1 : 1;
+        }).get();
+    }
 }
