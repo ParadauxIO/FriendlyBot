871e55c61118888ca62f1849be86a45e756272b9
Rían Errity <rian@paradaux.io>
2021-06-10 00:57:00 +0100

    Finished placeholder setup command

--- src/main/java/io/paradaux/friendlybot/commands/staff/technician/SetupCommand.java
+++ src/main/java/io/paradaux/friendlybot/commands/staff/technician/SetupCommand.java
@@ -2,78 +2,72 @@ package io.paradaux.friendlybot.commands.staff.technician;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import io.paradaux.friendlybot.managers.GuildSettingsManager;
 import io.paradaux.friendlybot.managers.PermissionManager;
-import io.paradaux.friendlybot.managers.RoleManager;
-import io.paradaux.friendlybot.utils.StringUtils;
 import io.paradaux.friendlybot.utils.models.configuration.ConfigurationEntry;
+import io.paradaux.friendlybot.utils.models.database.GuildSettingsEntry;
 import io.paradaux.friendlybot.utils.models.types.PrivilegedCommand;
-import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.entities.PermissionOverride;
+import net.dv8tion.jda.api.entities.TextChannel;
 import org.slf4j.Logger;
 
 public class SetupCommand extends PrivilegedCommand {
 
     private final EventWaiter waiter;
+    private final GuildSettingsManager guilds;
 
     public SetupCommand(ConfigurationEntry config, Logger logger, PermissionManager permissionManager, EventWaiter waiter) {
         super(config, logger, permissionManager);
         this.waiter = waiter;
         this.name = "setup";
         this.help = "Configures guild settings";
+        this.guilds = GuildSettingsManager.getInstance();
     }
 
     @Override
     protected void execute(CommandEvent event) {
+
+        Guild guild = event.getGuild();
+
         if (!isStaff(event.getGuild(), event.getAuthor().getId())) {
             return;
         }
 
-        event.reply("Beginning to configure this guild.");
-
-        EmbedBuilder builder = new EmbedBuilder();
-
-        event.getChannel().sendMessage("Are you going to use the custom color roles? (yes, no)").queue(message -> waiter.waitForEvent(MessageReceivedEvent.class,
-                messageEvent -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent -> {
-            boolean usingColorRoles = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
-
-            if (usingColorRoles) {
-                messageEvent.getChannel().sendMessage("Creating a roll called `Color Roles Begin Here`. Please move this role above where you would like color roles to be put. **N.B**: This has to be below the bot's highest role.)");
-                RoleManager.getInstance().setupColorRolePivot(event.getGuild());
-            }
-                event.getChannel().sendMessage("Are you going to use Email Verification? (yes, no)").queue(message2 -> waiter.waitForEvent(MessageReceivedEvent.class,
-                    messageEvent2 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent2 -> {
-                boolean usingEmail = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
-                    event.getChannel().sendMessage("Are you going to use the ModMail ? (yes, no)").queue(message3 -> waiter.waitForEvent(MessageReceivedEvent.class,
-                            messageEvent3 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent3 -> {
-                    boolean usingModMail = StringUtils.parseBoolean(messageEvent.getMessage().getContentRaw());
-
-                            event.getChannel().sendMessage("Are you going to use the custom color roles? (yes, no)").queue(message4 -> waiter.waitForEvent(MessageReceivedEvent.class,
-                                    messageEvent4 -> sameOrigin(event.getMessage(), messageEvent.getMessage()), messageEvent4 -> {
-
-                                    }));
-
-
-                    }));
-
-                }));
-
-
-            // Going to use roles
-            // Going to use modmail
-            // Going to use email verification
-
-
-        }));
-    }
-
-
-    public boolean sameOrigin(Message message1, Message message2) {
-        if (!message1.getChannel().getId().equals(message2.getChannel().getId())) {
-            return false;
+        if (guilds.guildExists(guild.getId())) {
+            event.reply("This guild has already been setup.");
+            return;
         }
 
-        return message1.getAuthor().getId().equals(message2.getAuthor().getId());
-    }
+        event.reply("Beginning to configure this guild.");
+        event.reply("This will create several channels which you can move around or rename, but they must remain the same channels");
 
+        guild.createCategory("FriendlyBot Administration").queue((category) -> {
+            // Make it so it isn't visible to the public
+            category.createPermissionOverride(guild.getPublicRole()).setDeny(Permission.MESSAGE_READ).queue((action) -> {
+                // Create the feature channels
+                category.createTextChannel("modmail").queue((modmail) -> {
+                    category.createTextChannel("modmail-output").queue((modmailOutput) -> {
+                        category.createTextChannel("auditlog").queue((auditlog) -> {
+                            category.createTextChannel("auditlog-private").queue((auditlogPrivate) -> {
+                                category.createTextChannel("message-logs").queue((messageLogs) -> {
+                                    GuildSettingsEntry guildEntry = guilds.createNewProfile(guild.getId())
+                                            .setModmailInputChannel(modmail.getId())
+                                            .setModmailOutputChannel(modmailOutput.getId())
+                                            .setPublicAuditLogChannel(auditlog.getId())
+                                            .setPrivateAuditLogChannel(auditlogPrivate.getId())
+                                            .setMessageLogChannel(messageLogs.getId());
+
+                                    guilds.updateProfile(guildEntry);
+                                    event.reply("Setup complete. Do not delete these channels. All channels are under FriendlyBot Administration and cannot be seen by anyone except Server Administrators.");
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
 }
