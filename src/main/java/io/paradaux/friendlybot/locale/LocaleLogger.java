8bad52de2e885b15ebd0d4a2354a38d268a1e99b
Rían Errity <rian@paradaux.io>
2021-08-01 22:44:05 +0100

    WIP Migrating Locale System from other project

---
+++ src/main/java/io/paradaux/friendlybot/locale/LocaleLogger.java
@@ -0,0 +1,40 @@
+package io.paradaux.friendlybot.locale;
+
+import org.slf4j.Logger;
+
+/**
+ * A wrapper on top of SLF4J which allows for the logging of system messages using locale entries to provide easier internationalisation.
+ * @author Rían Errity
+ * */
+public class LocaleLogger {
+
+    private static Logger logger;
+    private static boolean debug;
+
+    public LocaleLogger(Logger logger, boolean debug) {
+        LocaleLogger.logger = logger;
+        LocaleLogger.debug = debug;
+    }
+
+    public static void toggleDebug() {
+        debug = !debug;
+    }
+
+    public static void info(String str, String... args) {
+        logger.info(LocaleManager.get(str), (Object[]) args);
+    }
+
+    public static void warn(String str, String... args) {
+        logger.warn(LocaleManager.get(str), (Object[]) args);
+    }
+
+    public static void debug(String str, String... args) {
+        if (debug) {
+            logger.debug(LocaleManager.get(str), (Object[]) args);
+        }
+    }
+
+    public static void error(String str, String... args) {
+        logger.error(str, (Object[]) args);
+    }
+}
---
+++ src/main/java/io/paradaux/friendlybot/locale/LocaleManager.java
@@ -0,0 +1,39 @@
+package io.paradaux.friendlybot.locale;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+/**
+ * Utility to loading and holding singleton instances of the ResourceBundle which serves as the locale/internationalisation system for the
+ * plugin.
+ * @author Rían Errity
+ * */
+public class LocaleManager {
+
+    private static final File locale = new File("");
+
+    private static ResourceBundle bundle;
+
+    public LocaleManager() {
+        if (!locale.exists()) {
+
+        }
+
+        try (FileInputStream fis = new FileInputStream(locale)) {
+            bundle = new PropertyResourceBundle(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key) {
+        if (!bundle.containsKey(key)) {
+            return key;
+        }
+
+        return bundle.getString(key);
+    }
+}
