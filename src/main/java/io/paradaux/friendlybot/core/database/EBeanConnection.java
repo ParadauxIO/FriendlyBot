91233f4551cab28c67fb8104a47ddebbff163865
Rían Errity <rian@paradaux.io>
2021-08-02 13:47:56 +0100

    WIP EBean connection

---
+++ src/main/java/io/paradaux/friendlybot/core/database/EBeanConnection.java
@@ -0,0 +1,23 @@
+package io.paradaux.friendlybot.core.database;
+
+import io.ebean.Database;
+import io.ebean.DatabaseFactory;
+import io.ebean.config.DatabaseConfig;
+import io.ebean.datasource.DataSourceConfig;
+
+public class EBeanConnection {
+
+    private final DatabaseConfig config = new DatabaseConfig();
+    private final Database database;
+
+    public EBeanConnection() {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUsername("sa");
+        dataSourceConfig.setPassword("");
+        dataSourceConfig.setUrl("jdbc:h2:mem:myapp;");
+
+        this.config.setDataSourceConfig(dataSourceConfig);
+        this.database = DatabaseFactory.create(config);
+    }
+
+}
