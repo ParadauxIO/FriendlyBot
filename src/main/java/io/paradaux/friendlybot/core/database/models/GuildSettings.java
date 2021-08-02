8f5a7f2bfca223aa74f4a9481af9ce4d26a6d273
Rían Errity <rian@paradaux.io>
2021-08-02 13:47:21 +0100

    Setup EBean tests

---
+++ src/main/java/io/paradaux/friendlybot/core/database/models/GuildSettings.java
@@ -0,0 +1,34 @@
+package io.paradaux.friendlybot.core.database.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class GuildSettings {
+
+    @Id
+    String guildId;
+
+    String guildName;
+
+    public String getGuildId() {
+        return guildId;
+    }
+
+    public String getGuildName() {
+        return guildName;
+    }
+
+    public void setGuildId(String guildId) {
+        this.guildId = guildId;
+    }
+
+    public void setGuildName(String guildName) {
+        this.guildName = guildName;
+    }
+
+    @Override
+    public String toString() {
+        return "GuildSettings{" + "guildId='" + guildId + '\'' + ", guildName='" + guildName + '\'' + '}';
+    }
+}
