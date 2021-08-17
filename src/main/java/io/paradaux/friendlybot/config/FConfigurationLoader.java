fbc20142472e58ecabf995ef88a06d9a88828406
Rían Errity <rian@paradaux.io>
2021-08-17 12:16:48 +0100

    Fix https://github.com/SpongePowered/Configurate/issues/210

--- src/main/java/io/paradaux/friendlybot/config/FConfigurationLoader.java
+++ src/main/java/io/paradaux/friendlybot/config/FConfigurationLoader.java
@@ -1,10 +1,12 @@
 package io.paradaux.friendlybot.config;
 
+import io.paradaux.friendlybot.core.utils.IOUtils;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
 import org.spongepowered.configurate.serialize.SerializationException;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -19,6 +21,8 @@ public class FConfigurationLoader {
     }
 
     public static FConfiguration loadConfig() throws SerializationException {
+        deployConfig();
+
         loader = HoconConfigurationLoader.builder()
                 .path(Path.of("friendlybot.conf"))
                 .build();
@@ -39,8 +43,14 @@ public class FConfigurationLoader {
     }
 
     public static void saveConfig() throws ConfigurateException {
-        root.set(FConfiguration.class, root);
+        root.set(FConfiguration.class, config);
         loader.save(root);
+    }
+
+    private static void deployConfig() {
+        if (!new File("friendlybot.conf").exists()) {
+            IOUtils.exportResource("/friendlybot.conf",System.getProperty("user.dir") + "/friendlybot.conf");
+        }
     }
 
 }
