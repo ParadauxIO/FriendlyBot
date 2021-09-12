5909259b9fa8345b949d12dfd06fd5a62861f4f9
Rían Errity <rian@paradaux.io>
2021-09-12 21:02:12 +0100

    Update GuildSettingsTest.java

--- src/test/java/io/paradaux/friendlybot/database/GuildSettingsTest.java
+++ src/test/java/io/paradaux/friendlybot/database/GuildSettingsTest.java
@@ -1,8 +1,11 @@
 package io.paradaux.friendlybot.database;
 
 import io.ebean.DB;
+import io.paradaux.friendlybot.core.data.database.models.FGuild;
 import io.paradaux.friendlybot.core.data.database.models.GuildSettings;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class GuildSettingsTest {
 
@@ -21,5 +24,34 @@ public class GuildSettingsTest {
 
         // delete the customer
         DB.delete(settings);
+    }
+
+    @Test
+    public void fGuildTest() {
+        FGuild guild = new FGuild();
+        guild.setGuildId("778219443737329684");
+        guild.setCommandPrefix(";");
+
+        guild.setVerificationRoleId("781365795237527602");
+        guild.setVerificationInputId("886628377564430387");
+
+        guild.setAuditLogId("852334545750851624");
+        guild.setModAuditLogId("852334546627723294");
+
+        guild.setModMailInId("852334544316923905");
+        guild.setModMailOutId("852334545104928828");
+
+        guild.setMessageLogId("852334547105218591");
+
+        guild.setModerators(new ArrayList<>());
+        guild.setAdministrators(new ArrayList<>());
+
+
+        guild.setLastIncidentId(0);
+        guild.setLastTicketId(0);
+
+        DB.save(guild);
+
+
     }
 }
