020ca0d528b7637944878fa9e7dc9394ebe0fba1
Rían Errity <rian@paradaux.io>
2021-09-12 01:11:44 +0100

    Fix nullable possibility

--- src/main/java/io/paradaux/friendlybot/bot/commands/CatCommand.java
+++ src/main/java/io/paradaux/friendlybot/bot/commands/CatCommand.java
@@ -1,5 +1,6 @@
 package io.paradaux.friendlybot.bot.commands;
 
+import io.paradaux.friendlybot.bot.command.Command;
 import io.paradaux.friendlybot.bot.command.CommandBody;
 import io.paradaux.friendlybot.bot.command.DiscordCommand;
 import io.paradaux.friendlybot.data.database.models.FGuild;
@@ -10,7 +11,9 @@ import org.json.JSONArray;
 
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
+@Command(name="cat", description = "Get yourself a cute cat photo!")
 public class CatCommand extends DiscordCommand {
 
     private static final String CAT_API = "https://api.thecatapi.com/v1/images/search";
@@ -19,7 +22,13 @@ public class CatCommand extends DiscordCommand {
         HttpApi http = new HttpApi(getLogger());
         HttpRequest request = http.plainRequest(CAT_API);
 
-        http.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenAccept((response) -> {
+        CompletableFuture<HttpResponse<String>> responseFtr = http.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        if (responseFtr == null) {
+            throw new IllegalStateException("Cat: Response was null");
+        }
+
+        responseFtr.thenAccept((response) -> {
             EmbedBuilder builder = new EmbedBuilder()
                     .setImage(new JSONArray(response.body()).getJSONObject(0).getString("url"))
                     .setColor(NumberUtils.randomColor())
