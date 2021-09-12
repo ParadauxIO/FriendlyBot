b7d87d1180fee59bbdfc229cf02f7040445d9170
Rían Errity <rian@paradaux.io>
2021-09-12 16:48:28 +0100

    Re-enable test

--- src/test/java/io/paradaux/friendlybot/database/GuildSettingsTest.java
+++ src/test/java/io/paradaux/friendlybot/database/GuildSettingsTest.java
@@ -1,23 +1,25 @@
 package io.paradaux.friendlybot.database;
 
+import io.ebean.DB;
+import io.paradaux.friendlybot.core.data.database.models.GuildSettings;
 import org.junit.jupiter.api.Test;
 
 public class GuildSettingsTest {
 
     @Test
     public void insertFindDelete() {
-//        GuildSettings settings = new GuildSettings();
-//        settings.setGuildId("1");
-//        settings.setGuildName("This is a test");
-//
-//        // insert the customer in the DB
-//        DB.save(settings);
-//
-//        // Find by Id
-//        GuildSettings foundHello = DB.find(GuildSettings.class, "1");
-//        System.out.println(foundHello);
-//
-//        // delete the customer
-//        DB.delete(settings);
+        GuildSettings settings = new GuildSettings();
+        settings.setGuildId("1");
+        settings.setGuildName("This is a test");
+
+        // insert the customer in the DB
+        DB.save(settings);
+
+        // Find by Id
+        GuildSettings foundHello = DB.find(GuildSettings.class, "1");
+        System.out.println(foundHello);
+
+        // delete the customer
+        DB.delete(settings);
     }
 }
