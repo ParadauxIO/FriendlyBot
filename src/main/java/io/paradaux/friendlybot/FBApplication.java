2532fb531d95927a60607d99fa85c86ab8605836
Rían Errity <rian@paradaux.io>
2021-09-12 21:02:08 +0100

    Connect to database

--- src/main/java/io/paradaux/friendlybot/FBApplication.java
+++ src/main/java/io/paradaux/friendlybot/FBApplication.java
@@ -1,15 +1,19 @@
 package io.paradaux.friendlybot;
 
+import io.ebean.DB;
 import io.paradaux.friendlybot.bot.FBot;
-import io.paradaux.friendlybot.data.config.FConfiguration;
-import io.paradaux.friendlybot.data.config.FConfigurationLoader;
+import io.paradaux.friendlybot.core.data.config.FConfiguration;
+import io.paradaux.friendlybot.core.data.config.FConfigurationLoader;
 import io.paradaux.friendlybot.core.cache.GuildCache;
+import io.paradaux.friendlybot.core.data.database.EBeanConnection;
+import io.paradaux.friendlybot.core.data.database.models.FGuild;
 import io.paradaux.friendlybot.core.locale.LocaleLogger;
 import io.paradaux.friendlybot.core.locale.LocaleManager;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.configurate.ConfigurateException;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 
 public class FBApplication {
 
@@ -18,6 +22,7 @@ public class FBApplication {
     private static FConfiguration config;
     private static FBot bot;
     private static GuildCache guildCache;
+    private static EBeanConnection eBeanConnection;
 
 
     public static void main(String[] args) throws ConfigurateException, LoginException {
@@ -25,6 +30,7 @@ public class FBApplication {
         logger = new LocaleLogger(LoggerFactory.getLogger(FBApplication.class), true);
 
         config = FConfigurationLoader.loadConfig();
+        eBeanConnection = new EBeanConnection(config);
         bot = new FBot(config);
         guildCache = new GuildCache(bot.getClient());
     }
